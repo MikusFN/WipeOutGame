c86ff10fb77b12d4185dcaf75cbb3e341e4c240c
Fernando Camilo <a13233@alunos.ipca.pt>
2018-11-08 20:10:22 +0000

    Initial Stage Game objects

---
+++ Assets/Scripts/ThrusterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterController : MonoBehaviour
+{
+    public float strenght, distance;
+    public Transform[] thrusters;
+
+    RaycastHit hit;
+    Vector3 downForce;
+    Rigidbody rigidbody;
+    float distancePercent;
+
+    void start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        foreach (Transform thruster in thrusters)
+        {
+            if(Physics.Raycast(thruster.position, -thruster.up, out hit, distance))
+            {
+                distancePercent = 1 - (hit.distance / distance);
+
+                downForce = transform.up * strenght * distancePercent;
+
+                if (rigidbody)
+                {
+                    downForce = downForce * Time.deltaTime * rigidbody.mass;
+
+                    rigidbody.AddForceAtPosition(downForce, thruster.position);
+                }
+            }
+        }    
+
+    }
+}
