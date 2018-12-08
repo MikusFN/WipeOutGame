0d6303b3f8632db3f79a7a12cd03e8ce02593c09
Fernando Camilo <a13233@alunos.ipca.pt>
2018-12-08 19:22:04 +0000

    Spline Editor 2 Curves with Chosseable Handles

--- Assets/Scripts/Spline Editor/Editor/SplineInspector.cs
+++ Assets/Scripts/Spline Editor/Editor/SplineInspector.cs
@@ -11,8 +11,15 @@ public class SplineInspector : Editor
     private Transform handlePosition;
     private Quaternion handleRotation;
 
-    private const int lineSteps = 100;
-    private const float directionScale = 1.8f;
+    //Constantes que definem as linhas em cada curva e o tamanho das linhas de velocidade
+    private const int curveSteps = 10;
+    private const float directionScale = 2f;
+
+    //Constantes que definem o tamanho do butao de handle
+    private const float handleSize = 0.04f;
+    private const float pickSize = .06f;
+    //Para nao colocar nenhum handle selected by default
+    private int selectedIndex = -1;
     #endregion Fields
 
     #region Properties
@@ -30,7 +37,7 @@ public class SplineInspector : Editor
         DrawDefaultInspector();
         spline = target as BezierSpline;
 
-        if(GUILayout.Button("Add Curve"))
+        if (GUILayout.Button("Add Curve"))
         {
             Undo.RecordObject(spline, "Add curve");
             spline.AddCurve();
@@ -44,62 +51,91 @@ public class SplineInspector : Editor
         handlePosition = spline.transform;
         handleRotation = Tools.pivotRotation == PivotRotation.Local ?
             handlePosition.rotation : Quaternion.identity;
-
+        #region Old Version
         //A funcao Show point serve para tranformar o ponto em world space,
         //Verificar se houve modificações nos handle,
         //Caso haja atribui ao ponto em world space e por fim passa novamente para local space
         //Assim temos uma posiçao modificada de um ponto.
+        //for (int j = 0; j < spline.points.Length-1; j+=3)
+        //{
+        //    Vector3 p0 = ShowPoint(j+0);
+        //    Vector3 p1 = ShowPoint(j+1);
+        //    Vector3 p2 = ShowPoint(j+2);
+        //    Vector3 p3 = ShowPoint(j+3);
+
+
+        //    Handles.color = Color.gray;
+        //    Handles.DrawLine(p0, p1);
+        //    //Handles.DrawLine(p1, p2);
+        //    Handles.DrawLine(p2, p3);
+
+        //    Vector3 lineStart = spline.GetPointCubic(j);
+        //    Handles.color = Color.green;
+        //    Handles.DrawLine(lineStart, lineStart + spline.GetDirection(j));
+
+        //    //Numero de linhas que são calculadas (curveSteps)
+        //    for (int i = 1; i <= curveSteps; i++)
+        //    {
+        //        // Ponto na curva a cada step da linha
+        //        Vector3 lineEnd = spline.GetPointCubic(i / (float)curveSteps);
+        //        Handles.color = Color.white;
+        //        Handles.DrawLine(lineStart, lineEnd);
+        //        //Handles.color = Color.green;
+        //        //Direcções que parte do ponto que acaba a linha + a primeira derivada que da a velocidade a cada ponto.
+        //        //Handles.DrawLine(lineEnd, lineEnd + spline.GetDirectionCubic(i / (float)curveSteps) * directionScale);
+        //        // O ponto anterior de fim é o novo inicio
+        //        lineStart = lineEnd;
+        //    }
+        //    Handles.color = Color.green;
+        //    ShowDirections();
+        #endregion
+        //Ponto inicial
         Vector3 p0 = ShowPoint(0);
-        Vector3 p1 = ShowPoint(1);
-        Vector3 p2 = ShowPoint(2);
-        Vector3 p3 = ShowPoint(3);
-
-
-        Handles.color = Color.gray;
-        Handles.DrawLine(p0, p1);
-        //Handles.DrawLine(p1, p2);
-        Handles.DrawLine(p2, p3);
-
-
-        Vector3 lineStart = spline.GetPoint(0f);
-        Handles.color = Color.green;
-        Handles.DrawLine(lineStart, lineStart + spline.GetDirection(0f));
-
-        //Numero de linhas que são calculadas (lineSteps)
-        for (int i = 1; i <= lineSteps; i++)
+        for (int i = 1; i < spline.points.Length; i += 3)//Go through all the splines (a cada 3 pontos)
         {
-            // Ponto na curva a cada step da linha
-            Vector3 lineEnd = spline.GetPointCubic(i / (float)lineSteps);
-            Handles.color = Color.white;
-            Handles.DrawLine(lineStart, lineEnd);
-            //Handles.color = Color.green;
-            //Direcções que parte do ponto que acaba a linha + a primeira derivada que da a velocidade a cada ponto.
-            //Handles.DrawLine(lineEnd, lineEnd + spline.GetDirectionCubic(i / (float)lineSteps) * directionScale);
-            // O ponto anterior de fim é o novo inicio
-            lineStart = lineEnd;
+            //Tres pontos que fazem uma spline
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i + 1);
+            Vector3 p3 = ShowPoint(i + 2);
+
+            //Linhas que ligam os pontos de ocntrolo
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            //Curve Bezier
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+
+            //Ultimo ponto da anterior é o primeiro da proxima
+            p0 = p3;
         }
-        Handles.color = Color.green;
         ShowDirections();
     }
 
-
     private Vector3 ShowPoint(int index)
     {
-        if (index >= spline.Points.Length && index > 0)
-            index = spline.Points.Length - 1;
-
         //Transforma um ponto de local para world
         Vector3 point = handlePosition.TransformPoint(spline.Points[index]);
-        EditorGUI.BeginChangeCheck();
-
-        //Modifica o ponto no seu world space de acordo com o handle
-        point = Handles.DoPositionHandle(point, handleRotation);
-        if (EditorGUI.EndChangeCheck())
+        //Para obter sempre o mesmo tamanho para as handles (independente do tamanho d ecra)
+        float size = HandleUtility.GetHandleSize(point);
+        //Verifica primeiro qual é o butão selecionado
+        if (Handles.Button(point, handleRotation, handleSize * size, pickSize * size, Handles.DotCap))
         {
-            Undo.RecordObject(spline, "Move Point");
-            EditorUtility.SetDirty(spline);
-            //Modifica o ponto no seu local space
-            spline.Points[index] = handlePosition.InverseTransformPoint(point);
+            selectedIndex = index;
+        }
+        //So pode ser alturado o que esta actualmente selecionado
+        if (selectedIndex == index)
+        {
+            EditorGUI.BeginChangeCheck();
+            //Modifica o ponto no seu world space de acordo com o handle
+            point = Handles.DoPositionHandle(point, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(spline, "Move Point");
+                EditorUtility.SetDirty(spline);
+                //Modifica o ponto no seu local space
+                spline.Points[index] = handlePosition.InverseTransformPoint(point);
+            }
         }
         return point;
     }
@@ -108,13 +144,16 @@ public class SplineInspector : Editor
     private void ShowDirections()
     {
         Handles.color = Color.green;
-        Vector3 point = spline.GetPointCubic(0f);
+        Vector3 point = spline.GetPoint(0f);
+        //Velocidade non primeiro ponto da spline
         Handles.DrawLine(point, point + spline.GetDirectionCubic(0f) * directionScale);
-
-        for (int i = 1; i <= lineSteps; i++)
+        //O numero de linhas de velocidade a cada curva
+        int steps = curveSteps * 10 * spline.CurveCount;
+        for (int i = 1; i < steps; i++)
         {
-            point = spline.GetPointCubic(i / (float)lineSteps);
-            Handles.DrawLine(point, point + spline.GetDirectionCubic(i / (float)lineSteps) * directionScale);
+            point = spline.GetPoint(i / (float)steps);
+            //Direcções que parte do ponto que acaba a linha + a primeira derivada que da a velocidade a cada ponto
+            Handles.DrawLine(point, point + spline.GetDirectionCubic(i / (float)steps) * directionScale);
         }
     }
     #endregion Methods
--- Assets/Scripts/Spline Editor/MonoBehavior/BezierSpline.cs
+++ Assets/Scripts/Spline Editor/MonoBehavior/BezierSpline.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class BezierSpline: MonoBehaviour
+public class BezierSpline : MonoBehaviour
 {
     #region Fields
     public Vector3[] points;
+    private int curveCount;
+
     #endregion Fields
 
     #region Properties
     public Vector3[] Points
     { get { return points; } }
+
+    public int CurveCount { get { return (points.Length - 1 )/ 3; } } // Uma curve a cada 3 pontos.
 
     #endregion Properties
 
@@ -29,7 +33,6 @@ public class BezierSpline: MonoBehaviour
             new Vector3(-3,0,2),
             new Vector3(3,0,0),
             new Vector3(5,0,2)
-
         };
     }
 
@@ -40,21 +43,43 @@ public class BezierSpline: MonoBehaviour
         //Altera o tamanho do array de pontos para albergar 3 novos pontos
         Array.Resize(ref points, points.Length + 3);
 
-        for (int i = 3; i >0 ; i--)
+        for (int i = 3; i > 0; i--)
         {
             // Avança o ultimo ponto
             lastPoint.x = lastPoint.x + 1;
             //Coloca o novo ponto no array
-            lastPoint = points[points.Length - i];
+            points[points.Length - i] = lastPoint;
         }
     }
 
-    //Fucao que permite obter o ponto na curva entre o primeiro
+    //Funçao que permite obter o ponto na curva entre o primeiro
     //e o ultimo elementos do array de pontos de acordo com um valor t
     //Com o elemento intermedio a "puxar" a curva para si
     public Vector3 GetPoint(float t)
     {
-        return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], t));
+        //Indexador de pontos na curva
+        int i=-1;
+
+        //Valor maximo da curva (Quando o valor de f é superior a 1)
+        if (t >= 1f)
+        {
+            //Fica a 1
+            t = 1f;
+            //No fim da curva para obter os ultimos pontos
+            i = points.Length - 4;
+        }
+        else
+        {
+            //Precaver -se de valor negativos e  multiplica-se para saber em que parte da curva se esta. (0.7*8=5.6) -> 5 curva a 60% do caminho para 6
+            t = Mathf.Clamp01(t) * CurveCount;
+            //Guarda o valor da curva
+            i = (int)t;
+            //Guarda-se a percentagem de percurso da ultima curva
+            t -= i;
+            //Colocar o i de acordo com os pontos a indexar para estar na curva
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
     }
 
     public Vector3 GetPointCubic(float t)
@@ -64,7 +89,30 @@ public class BezierSpline: MonoBehaviour
 
     public Vector3 GetVelocity(float t)
     {
-        return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], t)) - transform.position;
+        //Indexador de pontos na curva
+        int i;
+
+        //Valor maximo da curva (Quando o valor de f é superior a 1)
+        if (t >= 1f)
+        {
+            //Fica a 1
+            t = 1f;
+            //No fim da curva para obter os ultimos pontos
+            i = points.Length - 4;
+        }
+        else
+        {
+            //Precaver -se de valor negativos e  multiplica-se para saber em que parte da curva se esta. (0.7*8=5.6) -> 5 curva a 60% do caminho para 6
+            t = Mathf.Clamp01(t) * CurveCount;
+            //Guarda o valor da curva
+            i = (int)t;
+            //Guarda-se a percentagem de percurso da ultima curva
+            t -= i;
+            //Colocar o i de acordo com os pontos a indexar para estar na curva
+            i *= 3;
+        }
+        //Obtem-se os tres pontos para as velocidades
+        return transform.TransformPoint(Bezier.GetFirstDerivative(points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
     }
     public Vector3 GetVelocityCubic(float t)
     {
